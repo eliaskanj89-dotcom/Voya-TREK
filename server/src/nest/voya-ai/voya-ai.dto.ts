485a6bbad11d7925b2050bf518c44dc0258c329a
eliaskanj89-dotcom <eliaskanj89@gmail.com>
2026-09-23 13:13:19 +0300

    feat: validate Voya day edit requests

--- server/src/nest/voya-ai/voya-ai.dto.ts
+++ server/src/nest/voya-ai/voya-ai.dto.ts
@@ -1,4 +1,4 @@
-import { voyaMaterializeDraftRequestSchema, voyaPlanDraftRequestSchema, voyaVerifyTripRequestSchema } from '@trek/shared';
+import { voyaApplyDayEditRequestSchema, voyaDayEditRequestSchema, voyaMaterializeDraftRequestSchema, voyaPlanDraftRequestSchema, voyaVerifyTripRequestSchema } from '@trek/shared';
 import { createZodDto } from 'nestjs-zod';
 
 export class VoyaPlanDraftDto extends createZodDto(voyaPlanDraftRequestSchema) {}
@@ -6,3 +6,7 @@ export class VoyaPlanDraftDto extends createZodDto(voyaPlanDraftRequestSchema) {
 export class VoyaMaterializeDraftDto extends createZodDto(voyaMaterializeDraftRequestSchema) {}
 
 export class VoyaVerifyTripDto extends createZodDto(voyaVerifyTripRequestSchema) {}
+
+export class VoyaDayEditDto extends createZodDto(voyaDayEditRequestSchema) {}
+
+export class VoyaApplyDayEditDto extends createZodDto(voyaApplyDayEditRequestSchema) {}
